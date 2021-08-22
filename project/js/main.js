10c2aa1bef26bdf0f86a51becc2adfda9dc25979
Vancouver <voron_pnevmatika@mail.ru>
2021-08-22 21:06:46 +0300

    Домашнее задание ко 2 уроку

--- project/js/main.js
+++ project/js/main.js
@@ -1,33 +1,72 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000, image: './img/notebook.jpg', alt: 'Фото ноутбука' },
-    { id: 2, title: 'Mouse', price: 20, image: './img/mouse.jpg', alt: 'Фото мышки' },
-    { id: 3, title: 'Keyboard', price: 200, image: './img/keyboard.jpg', alt: 'Фото клавиатуры' },
-    { id: 4, title: 'Gamepad', price: 50, image: './img/gamepad.jpg', alt: 'Фото геймпада' },
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+        this.calcProductsPrice();
+    }
 
-/**
- * Функция для формирования верстки каждого товара
- *
- * @param {Object} item
- * @return {string} 
- */
-const renderProduct = item => {
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <img src="${item.image}" alt="${item.alt}">
-                <p>Цена: ${item.price} руб.</p>
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000, image: './img/notebook.jpg', alt: 'Фото ноутбука' },
+            { id: 2, title: 'Mouse', price: 20, image: './img/mouse.jpg', alt: 'Фото мышки' },
+            { id: 3, title: 'Keyboard', price: 200, image: './img/keyboard.jpg', alt: 'Фото клавиатуры' },
+            { id: 4, title: 'Gamepad', price: 50, image: './img/gamepad.jpg', alt: 'Фото геймпада' },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+
+    calcProductsPrice() {
+        let productsPrice = 0;
+        for (let product of this.goods) {
+            productsPrice += product.price;
+        }
+        console.log(productsPrice);
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = product.image;
+        this.alt = product.alt;
+    }
+    render() {
+        return `<div class="product-item">
+                <h3>${this.title}</h3>
+                <img src="${this.img}" alt="${this.alt}">
+                <p>Цена: ${this.price} руб.</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
+}
 
-/**
- * Функция для добавления верстки всех товаров на страницу
- *
- * @param {Object} list
- */
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+class Cart {
+    constructor() {
+        this.addProduct();
+        this.removeProduct();
+        this.changeCartCount();
+        this.clearCart();
+        this.calcCartTotalPrice();
+    }
+}
 
-renderPage(products);
+class CartItem {
+    constructor() {
+        this.render();
+        this.calcItemCount();
+        this.calcItemTotalPrice();
+    }
+}
+
+let list = new ProductList();
